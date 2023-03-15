c2515f86200987c4783ab534e3c88d3088f326a0
Nick Sophinos <nick@wdeanmedical.com>
2023-03-14 21:57:28 -0400

    project creation

---
+++ MaComMaui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MaComMaui;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ MaComMaui/LoginPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MaComMaui;
+
+public partial class LoginPage : ContentPage
+{
+	public LoginPage()
+	{
+		InitializeComponent();
+	}
+}
