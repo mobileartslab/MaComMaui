72cddb5edaa31d1194fb110509699de33ee7fb50
Nick Sophinos <nick@wdeanmedical.com>
2023-05-05 13:04:38 -0400

    progress

--- MaComMaui/MauiProgram.cs
+++ MaComMaui/MauiProgram.cs
@@ -13,7 +13,13 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+        fonts.AddFont("Metropolis-Black.otf", "Metropolis Black");
+        fonts.AddFont("Metropolis-Light.otf", "Metropolis Light");
+        fonts.AddFont("Metropolis-Medium.otf", "Metropolis Medium");
+        fonts.AddFont("Metropolis-Regular.otf", "Metropolis Regular");
+        fonts.AddFont("Metropolis-Regular.otf", "Metropolis Regular");
+        fonts.AddFont("MaterialIcons-Regular.ttf", "Material Icons");
+      });
 
 #if DEBUG
 		builder.Logging.AddDebug();
