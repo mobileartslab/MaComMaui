7477f24747d3993de7f82cececa54c0974787f74
Nick Sophinos <nick@wdeanmedical.com>
2023-05-05 10:25:01 -0400

    progress

--- MaComMaui/MainPage.xaml.cs
+++ MaComMaui/MainPage.xaml.cs
@@ -1,25 +1,13 @@
-﻿namespace MaComMaui;
-
-public partial class MainPage : ContentPage
+﻿
+namespace MaComMaui
 {
-	int count = 0;
+  public partial class MainPage : ContentPage
+  {
+    public MainPage()
+    {
+      InitializeComponent();
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
-
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+      NavigationPage.SetHasNavigationBar(this, false);
+    }
+  }
 }
-
-
