208493f1846b62a397126d0ac8a28cb9d34758c9
Nick Sophinos <nick@wdeanmedical.com>
2023-06-04 00:01:49 -0400

    progress

--- MaComMaui/LoginPage.xaml.cs
+++ MaComMaui/LoginPage.xaml.cs
@@ -2,13 +2,31 @@
 
 public partial class LoginPage : ContentPage
 {
-	public LoginPage()
+  public event EventHandler<string> OnError;
+
+  public LoginPage()
 	{
 		InitializeComponent();
 	}
 
   async void Button_Clicked(System.Object sender, System.EventArgs e)
   {
+    if (emailValidator.IsNotValid)
+    {
+      foreach (var error in emailValidator.Errors)
+      {
+        OnError?.Invoke(sender, error.ToString());
+      }
+
+      return;
+    }
+
+    if (nameValidator.IsNotValid)
+    {
+      OnError?.Invoke(sender, "Name is required.");
+      return;
+    }
+
     await Shell.Current.GoToAsync("homeView");
   }
 }
